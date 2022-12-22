ab2e8ba2edb6d7325acc36296a727f244b3ded98
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-12-22 17:52:58 +0600

    React starte and reactivity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <h1>Redux Practice</h1>
+    <div className="card w-96 bg-neutral text-neutral-content m-auto mt-10">
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{count}</h2>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary" onClick={()=> setCount ((prvCount) => prvCount + 1)}>Accept</button>
+          <button className="btn btn-accent" onClick={()=> setCount ((prvCount) => prvCount -1)}>Deny</button>
+        </div>
+      </div>
     </div>
   );
 }
